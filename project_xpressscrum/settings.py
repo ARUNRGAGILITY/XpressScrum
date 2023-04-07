d1a1a189dc5f94e39d9d54bca1e437bb38f0db42
Arun R G <arunrg.agility@gmail.com>
2023-04-07 12:43:29 +0530

    fix1

--- project_xpressscrum/settings.py
+++ project_xpressscrum/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7=gw@od(c_1v_3sq&-33b79k$q$dhtlhf9z-s=mc8w-ne!(u2!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
