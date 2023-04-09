e166d51efc9e2f50a1e720dfba7a6a77b006f64c
ARUNRGAGILITY <96728746+ARUNRGAGILITY@users.noreply.github.com>
2023-04-09 17:19:45 +0530

    OSR PGDB

--- project_xpressscrum/settings.py
+++ project_xpressscrum/settings.py
@@ -64,14 +64,23 @@ WSGI_APPLICATION = 'project_xpressscrum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': os.environ.get("PGDATABASE"),
+       'USER': os.environ.get("PGUSER"),
+       'PASSWORD': os.environ.get("PGPASSWORD"),
+       'HOST': os.environ.get("PGHOST"),
+       'PORT': os.environ.get("PGPORT"),
+   }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
